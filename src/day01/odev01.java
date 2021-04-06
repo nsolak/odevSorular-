26d4eb8ca2ab5a99985ec15b08081e83f0f2c52d
nsolak <81630697+nsolak@users.noreply.github.com>
2021-04-06 14:00:23 +0300

    Update odev01.java

--- src/day01/odev01.java
+++ src/day01/odev01.java
@@ -12,6 +12,7 @@ public class odev01 {
          String pickName="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
          char iHarfi=pickName.toLowerCase().charAt(8);
          System.out.println(pickName.charAt(0)+" "+pickName.charAt(11)+" "+iHarfi);
+	System.out.println("buraya syso ekleme yaptım");	
 	}
 
 }
